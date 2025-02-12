81d3717d53f7ca0fbb9ae00af10ba72ac80ecbad
wisehaldi <liski.anna@gmail.com>
2025-02-12 23:40:08 +0200

    bash no text

--- convert.sh
+++ convert.sh
@@ -23,37 +23,3 @@ do
  
 done
 
-#########################################################################################
-### INFO ABOUT CONVERSION ###############################################################
-#########################################################################################
-
-#COMMAND FORM:
-#ffmpeg -activation_bytes XXXX -i audiobook.aax audiobook.mp3
-
-#https://kylepiira.com/2019/05/12/how-to-break-audible-drm/
-
-#########################################################
-#ACTIVATION BYTES (activation key from Audible):
-#https://audible-tools.kamsker.at/f92933e43390a900b8889efeade94fe3f6cbff44
-#AAX Checksum Resolver: 
-#f92933e43390a900b8889efeade94fe3f6cbff44
-#92bcc42e
-
-#REPLACE XXXX = 92bcc42e
-
-#########################################################
-#COMMAND with activation bytes:
-#ffmpeg -activation_bytes 92bcc42e -i audiobook.aax audiobook.mp3
-
-
-#########################################################
-#REAL EXAMPLE:
-#ffmpeg -activation_bytes 92bcc42e -i Deep-Survival-True-Stories-of-Miraculous-Endurance-and-Sudden-Death_ep6.aax Deep-Survival-True-Stories-of-Miraculous-Endurance-and-Sudden-Death_ep6.mp3
-
-#mkdir Deep-Survival
-#mv Deep-Survival-True-Stories-of-Miraculous-Endurance-and-Sudden-Death_ep6.aax Deep-Survival-True-Stories-of-Miraculous-Endurance-and-Sudden-Death_ep6.mp3  Deep-Survival/
-
-#########################################################
-#########################################################
-#########################################################
-
